c7c51545a58f4a1a650d06d8e599a2c21e2579eb
Pe-ngo <jamespengo95@gmail.com>
2022-08-18 13:44:44 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+    
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+
+greet_with_default("Sunny")
+
+def add(num1, num2)
+    num1 + num2
+end
+
+sum = add(1, 2)
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num / 2
+end
+
+halve("Four")
+
+halve(4)
+
